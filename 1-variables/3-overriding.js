404fe863fbf7d7a037538db1ea9963a98169cc17
Jyasveer Gotta <jgotta@redhat.com>
2017-07-23 13:03:24 +0530

    class for 23-july-2017

---
+++ 1-variables/3-overriding.js
@@ -0,0 +1,7 @@
+var a = 10;
+a = 20;
+console.log(a);
+
+var a = 'abc';
+a = 'jyasveer';
+console.log(a);
---
+++ 1-variables/5-random.js
@@ -0,0 +1,9 @@
+// 1. i have a variable. if it is a string i want to print as "it is a string" else i want "i dont know what it is"
+
+var test;
+
+if (typeof test == 'boolean') {
+    console.log('it is a string');
+} else {
+    console.log('i dont know what it is');
+}
